9915dace33e3ef593ba1d362045479c79f5e5d36
Jordan McQueen <j@jm.dev>
2026-01-29 11:04:59 +0900

    Add flake, rust-toolchain

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Rust dev environment";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+
+        billiards = rustPlatform.buildRustPackage {
+          pname = "billiards";
+          version = "0.4.1";
+          src = ./.;
+          cargoLock = { lockFile = ./Cargo.lock; };
+          nativeBuildInputs = [ ];
+          buildInputs = [ ];
+        };
+      in {
+        packages.default = billiards;
+        apps.default = flake-utils.lib.mkApp { drv = billiards; };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            llvmPackages.bolt
+
+            # Cargo checks / lints / tools
+            cargo-audit
+            cargo-deny
+            cargo-edit
+            cargo-license
+            cargo-pgo
+            cargo-udeps
+            cargo-watch
+            just
+          ];
+
+          shellHook = ''
+            # Tells rust-analyzer where the stdlib sources are
+            export RUST_SRC_PATH=${rustToolchain}/lib/rustlib/src/rust/library
+          '';
+        };
+      });
+}
